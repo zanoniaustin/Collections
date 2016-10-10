0cf3938b2eb5e186f6a7f9a72c8b0899a604b45a
zanoniaustin <zanoniaustin@yahoo.com>
2016-10-10 13:58:12 -0600

    Hash Set

--- Collections1/src/collections1/Collections1.java
+++ Collections1/src/collections1/Collections1.java
@@ -1,4 +1,17 @@
+/*
+    Created By: Austin Zanoni
+    Created: 10/10/2016
+    
+    
+    This program uses different types of collections and shows how different
+    they are and different ways to use them.
+
+*/
 package collections1;
+
+import static kiss.API.*;
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class Collections1 {
@@ -8,9 +21,31 @@ public class Collections1 {
         assert x == 0;
     }     
     
-    void testTreeMap() {
+    void testHashSet() {
         
+        // Showing that sets don't restore another value if there is a equal 
+        // value that is already in the set.
         
+        Set<String> uniqueNames = new HashSet<String>();
+        uniqueNames.add("john smith");
+        uniqueNames.add("john smith");
+        println(uniqueNames);
+        assert (uniqueNames.size() == 2) == false;
+        assert uniqueNames.size() == 1;
+        
+        uniqueNames.add("john tom");
+        assert uniqueNames.size() == 2;
+        
+        uniqueNames.add("susan peak");
+        uniqueNames.add("John Tom");
+        uniqueNames.add("john tom");
+        assert uniqueNames.size() == 4;
+        
+        try(Close out = outExpect("john smith", EOL, "susan peak", EOL, "john tom", EOL, "John Tom", EOL)){
+            for (String uniqueName : uniqueNames){
+                println(uniqueName);
+            }
+       }
     }
     
     
