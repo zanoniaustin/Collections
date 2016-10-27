be069b14f8e64b6a0f37d758087d4fcb7ad6a5c6
zanoniaustin <zanoniaustin@yahoo.com>
2016-10-27 11:48:40 -0600

    new no kiss file

---
+++ Collections1/src/collections1/noKiss.java
@@ -0,0 +1,15 @@
+
+package collections1;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.HashMap;
+
+
+public class noKiss {
+    
+}
