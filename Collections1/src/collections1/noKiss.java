fb191db5a40d6114f1e89923e07be1ec3943ef7f
zanoniaustin <zanoniaustin@yahoo.com>
2016-11-01 16:10:45 -0600

    code no kiss no tests

--- Collections1/src/collections1/noKiss.java
+++ Collections1/src/collections1/noKiss.java
@@ -1,4 +1,3 @@
-
 package collections1;
 
 import java.util.HashSet;
@@ -9,16 +8,61 @@ import java.util.Set;
 import java.util.TreeSet;
 import java.util.HashMap;
 
-
 public class noKiss {
-    
-    public static void main(String [] args){
-        
+
+    public static void main(String[] args) {
+
         // Hash Set
         Set<String> hsetNames;
         hsetNames = new HashSet<>();
         hsetNames.add("john smith");
         hsetNames.add("john smith");
         System.out.println(hsetNames);
+        
+        hsetNames.add("john tom");
+        
+        hsetNames.add("susan peak");
+        hsetNames.add("John Tom");
+        hsetNames.add("john tom");
+        System.out.println(hsetNames);
+        
+        
+        // Tree Set
+        Set<Integer> numbers;
+        numbers = new TreeSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(7);
+        numbers.add(0);
+        
+        numbers.add(10);
+        numbers.add(1);
+        numbers.remove(1);
+        
+        
+        // Hash Map
+        Map<String, Character> grades;
+        grades = new HashMap<>();
+        
+        grades.put("Betty", 'A');
+        grades.put("Betty", 'A');
+        grades.put("Betty", 'B');
+        
+        grades.put("Steven", 'C');
+        grades.put("Betty G", 'F');
+        
+        // Linked List
+        List<String> names;
+        names = new LinkedList<>();
+        names.add("Betty");
+        names.add("Steven");
+        names.add("Robert");
+        names.add("Betty");
+        
+        names.add("John");
+        
+        names.remove("Betty");
     }
+
 }
